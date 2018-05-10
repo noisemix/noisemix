9cd66fc5adcad2320f46c0a74f93a1d4ba951cb4
a-vrtn <vartanian.anooshik@gmail.com>
2018-05-10 15:51:29 +0400

    initial migration from gitlab.

---
+++ roadmap.md
@@ -0,0 +1,19 @@
+
+### Literature review
+
+TODO
+
+
+### Generation types
+
+TODO
+
+
+### Benchmarks
+
+TODO
+
+
+### Python library
+
+TODO: Release package on PyPI
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='noisemix',
+      version='0.1',
+      description='NoiseMix is a library for data generation for text datasets.',
+      url='https://gitlab.com/hetazotutyun/NoiseMix',
+      author='Anooshik Vartnian',
+      author_email='anooshik_vartanian@edu.aua.com',
+      license='MIT',
+      packages=['noisemix'],
+      zip_safe=False)
